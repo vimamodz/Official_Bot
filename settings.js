eb71a6fd3ad8d7ee0b44c2bcfc44646b79b7a715
vimamodz <125995272+vimamodz@users.noreply.github.com>
2023-07-09 14:06:35 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,73 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+//aumto presence update
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.autoblockmorroco = false //auto block 212 (true to on, false to off)
+global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
+global.antispam = false //auto kick spammer (true to on, false to off)
+
+
+//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
+global.keyopenai = "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"
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.owner = ['94740075693'] //ur owner number
+global.ownernomer = "94740075693" //ur owner number2
+global.ownername = "ɪᴛ ᴢᴢ ᴍᴇ 𝘒𝘢 𝘝𝘦𝘦 𝘚𝘏" //ur owner name
+global.ytname = "YT: ɪᴛ ᴢᴢ ᴍᴇ 𝘒𝘢 𝘝𝘦𝘦 𝘚𝘏" //ur yt chanel name
+global.socialm = "GitHub: ɪᴛ ᴢᴢ ᴍᴇ 𝘒𝘢 𝘝𝘦𝘦 𝘚𝘏" //ur github or insta name
+global.location = "India, Mizoram, Aizawl" //ur location
+
+//new
+global.botname = "ɪᴛ ᴢᴢ ᴍᴇ 𝘒𝘢 𝘝𝘦𝘦 𝘚𝘏"
+global.ownernumber = '94740075693'
+global.ownername = 'ɪᴛ ᴢᴢ ᴍᴇ 𝘒𝘢 𝘝𝘦𝘦 𝘚𝘏'
+global.ownerNumber = ["94740075693@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com"
+global.websitex = "https://youtu.be"
+global.wagc = "http://wa.me/94740075228?text=❝𝘏𝘌𝘠_𝘬𝘢𝘷𝘦𝘦_🩵💍🌈❞"
+global.themeemoji = '🪀'
+global.wm = " Bot Inc."
+global.botscript = 'https://github.com' //script link
+global.packname = "Sticker By ɪᴛ ᴢᴢ ᴍᴇ 𝘒𝘢 𝘝𝘦𝘦 𝘚𝘏"
+global.author = "🦄ɪᴛ ᴢᴢ ᴍᴇ 𝘒𝘢 𝘝𝘦𝘦 𝘚𝘏 \n\n+94740075693"
+global.creator = "94740075693@s.whatsapp.net"
+global.prefa = ['','!','.','#','&']
+global.sessionName = 'session'
+global.hituet = 0
+
+//media target
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+    success: 'Here you go!', 
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
